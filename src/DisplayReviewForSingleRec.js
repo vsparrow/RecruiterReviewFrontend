ca4e68510183ee63904e049a2a0c3093c27e2be2
vsparrow <khan@vsparrow.com>
2018-05-18 17:18:28 -0400

    Add rating to review

--- src/DisplayReviewForSingleRec.js
+++ src/DisplayReviewForSingleRec.js
@@ -4,17 +4,27 @@ class DisplayReviewForSingleRec extends Component {
 
   render(){
     let review = null;
+    let stars = null;
     if(this.props.review.review){
       console.log("GREATER THAN 0");
       console.log(this.props.review);
       review=this.props.review.review.slice(0,30)
       review += "..."
     }
+    // switch(this.props.review.rating){
+    //   case(5):
+    //     stars = ""
+    //     break;
+    //   default:
+    stars = "*".repeat(this.props.review.rating)
+    console.log(stars);
+    // }
+
     // console.log(review);
     return(
       <div className="DisplayReviewForSingleRec" >
-        DisplayReviewForSingleRec
         <h3>{review}</h3>
+        <span>Rating: {stars}</span>
       </div>
     )
   }

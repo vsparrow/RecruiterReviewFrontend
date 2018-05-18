d434d84165b818cdeaedeb61b8d059832662c7dd
vsparrow <khan@vsparrow.com>
2018-05-18 12:29:14 -0400

    DisplayRecruiters using DisplayRecruiterMinimal

--- src/DisplayRecruiterMinimal.js
+++ src/DisplayRecruiterMinimal.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class DisplayRecruiterMinimal extends Component {
+  handleClick = ()=>{
+    // this.props.handleClickOnRec(this.props.index)
+    this.props.handleClickOnRec(this.props.recruiter.id)
+  }
   render(){
+    console.log(this.props.recruiter);
     return(
-      <div className="DisplayRecruiterMinimal">
+      <div className="DisplayRecruiterMinimal" style={{borderStyle: "solid"}} onClick={this.handleClick}>
         DisplayRecruiterMinimal
+        <h2>{this.props.recruiter.firstname} {this.props.recruiter.lastname}</h2>
+        <h3>{this.props.recruiter.location}</h3>
       </div>
     )
   }
--- src/DisplayRecruiters.js
+++ src/DisplayRecruiters.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import DisplayRecruiterMinimal from './DisplayRecruiterMinimal'
 
 class DisplayRecruiters extends Component {
+  handleClickOnRec = (id)=>{
+    console.log("ID of clicked REC IS: " +id);
+  }
+
   render(){
     console.log("DisplayRecruiters");
     console.log(this.props.state);
     console.log("end ****");
-    const recruiters = this.props.state.recruiters.map((recruiter,index)=><div key={index}><DisplayRecruiterMinimal/></div> )
+    const recruiters = this.props.state.recruiters.map((recruiter,index)=><div key={index} ><DisplayRecruiterMinimal index={index} handleClickOnRec={this.handleClickOnRec} recruiter={recruiter}/></div> )
     return(
-      <div className="DisplayRecruiters">
+      <div className="DisplayRecruiters container" >
         DisplayRecruiters
         {recruiters}
       </div>

71946ca0987a52eb4d6da83b5eb06da46733bbd6
vsparrow <khan@vsparrow.com>
2018-05-18 17:04:09 -0400

    show only partial review text on recruiter#show page

--- src/DisplayReviewForSingleRec.js
+++ src/DisplayReviewForSingleRec.js
@@ -3,10 +3,18 @@ import React, { Component } from 'react';
 class DisplayReviewForSingleRec extends Component {
 
   render(){
+    let review = null;
+    if(this.props.review.review){
+      console.log("GREATER THAN 0");
+      console.log(this.props.review);
+      review=this.props.review.review.slice(0,30)
+      review += "..."
+    }
+    // console.log(review);
     return(
       <div className="DisplayReviewForSingleRec" >
         DisplayReviewForSingleRec
-        <h3>{this.props.review.review}</h3>
+        <h3>{review}</h3>
       </div>
     )
   }

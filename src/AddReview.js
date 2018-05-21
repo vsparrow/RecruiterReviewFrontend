12b2cbbe407ae5fd18fa910c5380aa7c45dc57fd
vsparrow <khan@vsparrow.com>
2018-05-21 11:10:28 -0400

    add begining of component AddReview

---
+++ src/AddReview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AddReview extends Component {
+
+  render(){
+    console.log(this.props.state);
+    return(
+      <div className="AddReview" >
+
+      </div>
+    )
+  }
+}
+export default AddReview
--- src/DisplayRecruiterFull.js
+++ src/DisplayRecruiterFull.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import DisplayReviewsForSingleRec from './DisplayReviewsForSingleRec'
-
+import AddReview from './AddReview'
 class DisplayRecruiterFull extends Component {
+  state = {addReview: false}
+
+  handleClick = ()=>{
+    // console.log(this.props.state);
+    this.setState({addReview: true})
+  }
 
   render(){
-    console.log(this.props.state);
+
     let recruiter=this.props.state.recruiters.find((r)=>{ return r.id === this.props.state.selectedRecruiterId})
-    console.log(recruiter);
+    let  sendState = {...this.state,...this.props.state}
     return(
       <div className="DisplayRecruiterFull" >
         DisplayRecruiterFull
@@ -16,8 +22,8 @@ class DisplayRecruiterFull extends Component {
         <h3>{recruiter.linkedin}</h3>
         <h3>{recruiter.website}</h3>
         <h3>{recruiter.location}</h3>
-
-        <DisplayReviewsForSingleRec state={this.props.state}/>
+        <button onClick={this.handleClick}>ADD REVIEW</button>
+        {this.state.addReview ? <AddReview state={sendState}/> : <DisplayReviewsForSingleRec state={this.props.state}/>}
       </div>
     )
   }

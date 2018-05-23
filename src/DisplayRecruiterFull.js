6d885082b1afa9daacba01ca583b59b4e867415d
vsparrow <khan@vsparrow.com>
2018-05-23 14:39:04 -0400

    update Add Review button

--- src/DisplayRecruiterFull.js
+++ src/DisplayRecruiterFull.js
@@ -19,16 +19,16 @@ class DisplayRecruiterFull extends Component {
     return(
       <div className="DisplayRecruiterFull " >
         <div className="wrapper-relatives">
-        <img className="img-recruiter" src={ require('./placeholder.png') }/>
-        <h1 style={{fontWeight: 800}}>{recruiter.firstname} {recruiter.lastname}</h1>
-        <h2>{recruiter.company}</h2>
-        <h3>{recruiter.email }</h3>
-        <h3>{recruiter.phonenumber}</h3>
-        <h3>{recruiter.linkedin}</h3>
-        <h3>{recruiter.website}</h3>
-        <h3>{recruiter.location}</h3>
+          <img className="img-recruiter" src={ require('./placeholder.png') }/>
+          <h1 style={{fontWeight: 800}}>{recruiter.firstname} {recruiter.lastname}</h1>
+          <h2>{recruiter.company}</h2>
+          <h3>{recruiter.email }</h3>
+          <h3>{recruiter.phonenumber}</h3>
+          <h3>{recruiter.linkedin}</h3>
+          <h3>{recruiter.website}</h3>
+          <h3>{recruiter.location}</h3>
         </div>
-        <button onClick={this.handleClick}>ADD REVIEW</button>
+        <button className="btn btn-success btn-large" onClick={this.handleClick}>ADD REVIEW</button>
 
         {this.state.addReview ? <AddReview state={sendState} cancelReview={this.cancelReview} fetchRecruiters={this.props.fetchRecruiters}/> : <DisplayReviewsForSingleRec state={this.props.state}/>}
       </div>

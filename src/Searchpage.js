02e3383dfbbd5ec65e704bc59021539225626ae5
vsparrow <khan@vsparrow.com>
2018-05-25 11:50:24 -0400

    add fetch of searchterm

--- src/Searchpage.js
+++ src/Searchpage.js
@@ -2,6 +2,25 @@
 import React, { Component } from 'react';
 
 class FirstPage extends Component {
+
+  componentDidMount(){
+    // console.log("componentDidMount");
+    this.fetchRecruiters()
+  }
+
+  fetchRecruiters = ()=>{
+    const url = this.props.state.url + "/search"
+    fetch(url,{
+      method: 'POST',
+      headers: {'Content-type' : 'application/json', authorization: this.props.state.authorization},
+      body:  JSON.stringify({search: this.props.state.searchterm})
+    })
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+    // .then(json=>{this.setState({recruiters: json},()=>{console.log(this.state)})
+    // .then(json=>{this.setState({recruiters: json})
+  }//fetchRecruiters
+
   render(){
     console.log(this.props);
     return(
